2f911a4dc1d14cabf4e725c3cc6bb913f2f6f0f4
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2018-09-30 21:26:47 +0200

    Added NetworkHudManager and NetworkUI

---
+++ Assets/Scripts/NetworkHudManager.cs
@@ -0,0 +1,19 @@
+﻿namespace UnityEngine.Networking
+{
+	[RequireComponent(typeof(NetworkManager))]
+	public class NetworkHudManager : MonoBehaviour
+	{
+		private NetworkManager _networkManager;
+
+		private void Awake()
+		{
+			_networkManager = GetComponent<NetworkManager>();
+		}
+
+		public void StartHost()
+		{
+			if (!NetworkServer.active && !NetworkClient.active)
+				_networkManager.StartHost();
+		}
+	}
+}
